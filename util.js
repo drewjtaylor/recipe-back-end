6c0faea42739f33d60f26b6d10fe19972ce51bbc
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-11-16 14:31:17 -0500

    ingredient search working

---
+++ util.js
@@ -0,0 +1,18 @@
+
+
+const translateToQuery = (input) => {
+    if (typeof input === String) {
+        return input
+    } else if (
+        typeof input === Array) {
+            let output = '';
+
+
+            // 
+        }
+}
+
+
+console.log(translateToQuery('Just a string'))
+
+console.log(translateToQuery(['apple', 'banana']))
